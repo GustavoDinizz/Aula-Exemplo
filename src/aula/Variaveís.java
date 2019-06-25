badc568ceade54e378333846dadca0993cb7ec15
GustavoDinizz <gustavo.silvaxrl82013@hotmail.com>
2019-06-25 12:24:04 -0300

    ok

---
+++ src/aula/Variaveís.java
@@ -0,0 +1,17 @@
+package aula;
+
+import javax.swing.JOptionPane;
+
+public class Variaveís {
+
+	public static void main(String[] args) {
+		int i = Integer.parseInt(JOptionPane.showInputDialog("Digite um número:"));
+		double d = Double.parseDouble(JOptionPane.showInputDialog("Digite um número"));
+		
+		// TODO Auto-generated method stub
+		System.out.println("i = " + i);
+		System.out.println("d =" + d);
+
+	}
+
+}
